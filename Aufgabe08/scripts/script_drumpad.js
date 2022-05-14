2aa4595372a8003e36e6311290e9e169e0a20da4
Dennis-Gruetzmacher <101892643+Dennis-Gruetzmacher@users.noreply.github.com>
2022-05-14 21:31:48 +0200

    CHA Aufgabe08 testing

--- Aufgabe08/scripts/script_drumpad.js
+++ Aufgabe08/scripts/script_drumpad.js
@@ -1,12 +1,10 @@
 /*****Variablen: Deklaration und Initialisierung******/
-
-var sounds: HTMLAudioElement[] = 
-[
+var sounds = [
     new Audio("assets/hihat.mp3"),
     new Audio("assets/kick.mp3"),
     new Audio("assets/snare.mp3"),
-    new Audio("assets/A.mp3"), 
-    new Audio("assets/C.mp3"), 
+    new Audio("assets/A.mp3"),
+    new Audio("assets/C.mp3"),
     new Audio("assets/F.mp3"),
     new Audio("assets/G.mp3"),
     new Audio("assets/laugh-1.mp3"),
@@ -14,15 +12,13 @@ var sounds: HTMLAudioElement[] =
 ];
 /***Setup und VErteilen der EventListener*************/
 const pads = document.querySelectorAll(".pad");
-for (let i = 0; i < pads.length; i++)
-{
-   pads[i].addEventListener("click", playSample);
+for (let i = 0; i < pads.length; i++) {
+    pads[i].addEventListener("click", playSample);
 }
 document.querySelector(".playbutton").addEventListener("click", playSample);
-
 /***********Funktionen***************** */
-function playSample()
-{
+function playSample() {
     let activeIndex = document.querySelector(".pad:hover").getAttribute("id");
     sounds[activeIndex].play();
 }
+//# sourceMappingURL=script_drumpad.js.map
