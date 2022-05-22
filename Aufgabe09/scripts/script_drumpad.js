901461b251b2a384fa76fdb8b5b0dbaa638ab938
Dennis-Gruetzmacher <101892643+Dennis-Gruetzmacher@users.noreply.github.com>
2022-05-22 23:28:58 +0200

    Cha Aufgabe09 testing

--- Aufgabe09/scripts/script_drumpad.js
+++ Aufgabe09/scripts/script_drumpad.js
@@ -1,8 +1,9 @@
 var Aufgabe09;
 (function (Aufgabe09) {
     var activeIndex;
-    var playbackID = 0;
-    var tempo = 100;
+    var playbackID;
+    var statePlayBeats = false;
+    var stateRecording = false;
     const sounds = [
         new Audio("assets/hihat.mp3"),
         new Audio("assets/kick.mp3"),
@@ -14,80 +15,108 @@ var Aufgabe09;
         new Audio("assets/laugh-1.mp3"),
         new Audio("assets/laugh-2.mp3")
     ];
-    const beats = [
-        new Audio("assets/hihat.mp3"),
-        new Audio("assets/kick.mp3"),
-        new Audio("assets/snare.mp3"),
-        new Audio("assets/kick.mp3")
-    ];
+    /*Reihenfolge des wiederholenden Beats*/
+    var beats = [0, 1, 2, 1];
     /***Setup und Verteilen der EventListener*************/
     const pads = document.querySelectorAll(".pad"); /*Übergibt Array aller Pads im document */
     for (let i = 0; i < pads.length; i++) { /*Schleife durchläuft Array und hängt an jedes element einen EventListener*/
-        pads[i].addEventListener("click", playSample);
+        pads[i].addEventListener("click", padClicked);
     }
-    document.querySelector("body").addEventListener("keypress", playSamplebyKeys);
-    document.querySelector("#playButton").addEventListener("click", repeatBeats);
-    document.querySelector("#stopButton").addEventListener("click", stopBeats);
-    document.querySelector("#tempoChangeUp").addEventListener("click", tempoUp);
-    document.querySelector("#tempoChangeDown").addEventListener("click", tempoDown);
+    /*EventListener am gesamten Dokument --> horcht auf Tastendruck*/
+    document.addEventListener("keypress", keyPressed);
+    /*EventListener am mainButton (Play/Stop)--> horcht auf klick*/
+    document.querySelector("#buttonMain").addEventListener("click", mainButtonPressed);
+    document.querySelector("#buttonRemix").addEventListener("click", doRemix);
+    document.querySelector("#buttonRecord").addEventListener("click", toogleRecording);
     /***********Funktionen******************/
-    function playSample() {
-        activeIndex = document.querySelector(".pad:hover").getAttribute("id"); /*übergibt aktiven Index des gehoverten Elements */
+    /*Abspielfunktion*/
+    function mainPlay(activeIndex) {
         sounds[activeIndex].play(); /*Spiele die Sounddatei des aktiven Index ab*/
     }
-    function playSamplebyKeys(beat) {
+    /*Pad clicked*/
+    function padClicked() {
+        activeIndex = parseInt(document.querySelector(".pad:hover").getAttribute("id"));
+        mainPlay(activeIndex);
+        console.log("clicked Pad: " + activeIndex);
+        if (stateRecording == true) {
+            beats.push(activeIndex);
+            console.log("Inhalt Array beats[]" + beats);
+        }
+    }
+    /*Taste gedrückt*/
+    function keyPressed(beat) {
         if (checkPressedKey(beat.key) == true) {
-            activeIndex = String((beat.key) - 1);
-            sounds[activeIndex].play();
+            activeIndex = (beat.key) - 1;
+            mainPlay(activeIndex);
+            if (stateRecording == true) {
+                beats.push(activeIndex);
+                console.log(beats);
+            }
         }
         else {
             return;
         }
     }
-    function repeatBeats() {
-        clearInterval(playbackID);
-        playbackID = setInterval(playBeats, tempo);
-    }
-    function playBeats() {
-        setTimeout(function () { beats[1].play(); }, tempo);
-        /*  setTimeout(function(){sounds[1].play() }, 250);
-          setTimeout(function(){sounds[1].play() }, 500);
-          setTimeout(function(){sounds[1].play() }, 750);
-          setTimeout(function(){sounds[1].play() }, 1000);
-          setTimeout(function(){sounds[2].play() }, 750);*/
-    }
-    function stopBeats() {
-        clearInterval(playbackID);
-    }
+    /*Prüfung ob richtige Taste gedrückt wurde*/
     function checkPressedKey(activeKey) {
         for (let i = 1; i < 10; i++) {
             if (activeKey == i) {
-                console.log(i);
+                console.log("Pressed key: " + i);
                 return true;
             }
         }
         console.log("Wrong key");
         return false;
     }
-    function tempoUp() {
-        console.log("core: " + tempo);
-        if (tempo < 2000) {
-            tempo += 50;
-            console.log("mod: " + tempo);
+    /*Abspielbutton gedrückt*/
+    function mainButtonPressed() {
+        if (statePlayBeats == false) {
+            document.getElementById("buttonMain").classList.remove("fa-play");
+            document.getElementById("buttonMain").classList.add("fa-pause");
+            statePlayBeats = true;
+            playbackID = setInterval(playBeats, beats.length * 250);
+            console.log("Status Playback: " + statePlayBeats);
+            console.log("PlaybackID: " + playbackID);
         }
         else {
-            tempo = 1000;
+            document.getElementById("buttonMain").classList.remove("fa-pause");
+            document.getElementById("buttonMain").classList.add("fa-play");
+            statePlayBeats = false;
+            clearInterval(playbackID);
         }
     }
-    function tempoDown() {
-        console.log("core: " + tempo);
-        if (tempo > 0) {
-            tempo -= 50;
-            console.log("mod: " + tempo);
+    /*Generierung des Beats auf GRundlage von Array beats[]*/
+    function playBeats() {
+        console.log("playBeats active");
+        for (let i = 0; i < beats.length; i++) {
+            setTimeout(function () { mainPlay(beats[i]); }, i * 250);
+        }
+    }
+    /*Leere Array beats und fülle es random neu*/
+    function doRemix() {
+        beats.length = 0;
+        let randomlength = getRandomNumberBetween(3, 8);
+        for (let j = 0; j < randomlength; j++) {
+            beats.push(getRandomNumberBetween(0, 2));
+        }
+        console.log("Neuer Beat im Array beats[]: " + beats);
+    }
+    function toogleRecording() {
+        if (stateRecording == false) {
+            beats.length = 0;
+            document.getElementById("buttonRecord").style.color = "#00ff00";
+            stateRecording = true;
+            console.log("Status Aufnahme: " + stateRecording);
         }
         else {
-            tempo = 1000;
+            document.getElementById("buttonRecord").style.color = "#ffffff";
+            stateRecording = false;
+            console.log("Status Aufnahme: " + stateRecording);
         }
+    }
+    /*Generiere Random nummer zwischen min und max*/
+    function getRandomNumberBetween(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 })(Aufgabe09 || (Aufgabe09 = {}));
 //# sourceMappingURL=script_drumpad.js.map
